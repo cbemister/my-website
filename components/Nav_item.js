d66e4321a2eca30855671781a99b3a100fd25473
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2018-07-24 23:22:39 -0600

    Added href to nav_item module

--- components/Nav_item.js
+++ components/Nav_item.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 export default ({ href, name }) =>
-  <Link passHref="true">
+  <Link href={href} passHref="true">
   <li className="nav-category js-nav-animate">
     <a href={href} className="nav-link">{name}</a>
   </li>
