5cec0acfa6e264e25a4c62f7d63feb8de519d5de
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2018-07-24 23:01:53 -0600

    Push remaining static pages live

--- out/_next/static/commons/manifest.js
+++ out/_next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "b29690b14785fa96870f"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "0f17769db18e916c71c6"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_document.js","2":"bundles\\pages\\_app.js","3":"bundles\\pages\\_error.js","6":"bundles\\pages\\index.js","7":"bundles\\pages\\blog.js"}[chunkId]||chunkId) + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_document.js","2":"bundles\\pages\\_app.js","3":"bundles\\pages\\_error.js","6":"bundles\\pages\\index.js","7":"bundles\\pages\\stack.js"}[chunkId]||chunkId) + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
