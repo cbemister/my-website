fd2a350ad8e6837bb6650babac00fb0eb55c5680
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2018-07-28 22:20:27 -0600

    Write homepage content

--- pages/index.js
+++ pages/index.js
@@ -16,19 +16,19 @@ const Index = ({ posts = [] }) =>
             </div>
 
             <div className="case-items">
-                <h2 className="section-header">Work.</h2>
+                <h2 className="section-header">Projects.</h2>
 
                 <div data-image="#" className="case-item">
-                    <span className="button-image-overlay">View project</span>
-                    <h3 className="case-item-header">Project Title</h3>
-                    <p className="case-item-text">Aliquip elit labore non id nulla dolore magna in ea tempor. Ad aliquip mollit et ullamco dolore culpa aliquip ut tempor velit aliquip adipisicing. Consequat reprehenderit veniam dolor sint velit dolor.</p>
-                    <a href="#" className="case-item-link"></a>
+                    {/* <span className="button-image-overlay">View project</span> */}
+                    <h3 className="case-item-header">Crawl to Dev Node.js App </h3>
+                    <p className="case-item-text">I built this app to create a local dev environment to streamline my development process when working on new page level website functionality.  I wanted an alternative to developing in browser, on the live website, or working with the limitations of the website staging environment.  The app has the following features & functionality: built using Node.js as CLI application, download an entire webpage's assets, and launch a local dev envivronment from it.  You can check it out on my Github page.   </p>
+                    <a href="https://github.com/cbemister/crawl-to-dev" target="_new" className="case-item-link"></a>
                 </div>
 
                 <div className="case-item">
-                    <span className="button-image-overlay">View project</span>
-                    <h3 className="case-item-header">Project Title</h3>
-                    <p className="case-item-text">Aliquip elit labore non id nulla dolore magna in ea tempor. Ad aliquip mollit et ullamco dolore culpa aliquip ut tempor velit aliquip adipisicing. Consequat reprehenderit veniam dolor sint velit dolor.</p>
+                    {/* <span className="button-image-overlay">View project</span> */}
+                    <h3 className="case-item-header">Vehicle Image Download App</h3>
+                    <p className="case-item-text">I built this app to make it easier for my design team to be able to download the vehicle specifications and photos of a specific stock number.  I built two version of this app: a version to download multiple photos of a single stock number or download a single photo for multiple stock numbers.  The app has the following features & functionality: built using AJAX as a single page app hosted on a live website, add-to-cart functionality, select one or more photos to download, and download vehicle specifications as CSV file. </p>  
                     <div className="case-item-content">
                         <div className="content-projectName"></div>
                     </div>
@@ -36,83 +36,27 @@ const Index = ({ posts = [] }) =>
                 </div>
 
                 <div data-image="#" className="case-item case-item-wide">
-                    <span className="button-image-overlay">View project</span>
-                    <h3 className="case-item-header">Project Title</h3>
+                    {/* <span className="button-image-overlay">View project</span> */}
+                    <h3 className="case-item-header">ChrisBemister.com Webiste</h3>
 
-                    <p className="case-item-text">projectName</p>
+                    <p className="case-item-text">I am building this website as both a portfolio project to showcase what I have built, and as a platform to help me build new website functionality and applications.  I am not always able to apply the advanced web development techniques that I have been learning at my current position. The website has the following features & functionality: built using Next.js to create a Reactbased server-side rendered app, uses Sanity.io to store the content in a headless CMS, hosted on Netlify static site hosting, automatic deployment through Zapier using web hooks, and Github.</p>
                     <a href="#" className="case-item-link"></a>
                 </div>
 
                 <div data-image="#" className="case-item">
-                    <span className="button-image-overlay">Read more</span>
-                    <h3 className="case-item-header">Project Title</h3>
-                    <p className="case-item-text">Aliquip elit labore non id nulla dolore magna in ea tempor. Ad aliquip mollit et ullamco dolore culpa aliquip ut tempor velit aliquip adipisicing. Consequat reprehenderit veniam dolor sint velit dolor.</p>
+                    {/* <span className="button-image-overlay">Read more</span> */}
+                    <h3 className="case-item-header">Promo JS App</h3>
+                    <p className="case-item-text">This is one of my oldest running applications. I have continuously worked to improve it as my development skills have improved and new functionality has been requested.  Originally, I built it to reduce the amount time it would take to perform - program change - a monthly switchover of the OEM marketing programs. The app has the following features & functionality: a JavaScript app built using the revealing module pattern, uses JSON format to store the data, run features before or after page load, has a built-in slideshow widget, and content personalization using URL parametres.</p>
                     <a href="#" className="case-item-link"></a>
                 </div>
 
                 <div className="case-item">
-                    <span className="button-image-overlay">View project</span>
-                    <h3 className="case-item-header">Project Title</h3>
-                    <p className="case-item-text">Aliquip elit labore non id nulla dolore magna in ea tempor. Ad aliquip mollit et ullamco dolore culpa aliquip ut tempor velit aliquip adipisicing. Consequat reprehenderit veniam dolor sint velit dolor.</p>
+                    {/* <span className="button-image-overlay">View project</span> */}
+                    <h3 className="case-item-header">Vehicle Listings Page</h3>
+                    <p className="case-item-text">I built this page with the goal of improving user experience and to increase VDP (Vehicle Detail Page) pageviews. The initial prototype was built using AJAX with limited functionality.  I had planned to rebuild it using a  front-end framework once I got an API set-up with the inventory feed. The page has the following features & functionality: built using AJAX as a single page app hosted on a live website, infinite scroll, basic filtering, sort inventory based on pageview data imported from a Google Sheet.</p>
                     <div className="case-item-content">
 
                     </div>
-                    <a href="#" className="case-item-link"></a>
-                </div>
-
-                <div data-image="#" className="case-item case-item-wide case-item-hi-curriculum">
-                    <span className="button-image-overlay">View program</span>
-                    <h3 className="case-item-header">Aute ipsum nisi anim nostrud fugiat</h3>
-                    <p className="case-item-text">Aliquip elit labore non id nulla dolore magna in ea tempor. Ad aliquip mollit et ullamco dolore culpa aliquip ut tempor velit aliquip adipisicing. Consequat reprehenderit veniam dolor sint velit dolor.</p>
-                    <a href="#" className="case-item-link"></a>
-                </div>
-
-                <div className="case-item">
-                    <span className="button-image-overlay">View project</span>
-                    <h3 className="case-item-header">Project Title</h3>
-                    <p className="case-item-text">Aliquip elit labore non id nulla dolore magna in ea tempor. Ad aliquip mollit et ullamco dolore culpa aliquip ut tempor velit aliquip adipisicing. Consequat reprehenderit veniam dolor sint velit dolor.</p>
-                    <div className="case-item-content">
-
-                    </div>
-                    <a href="#" className="case-item-link"></a>
-                </div>
-
-                <div data-image="#" className="case-item">
-                    <span className="button-image-overlay">View project</span>
-                    <h3 className="case-item-header">Project Title</h3>
-                    <p className="case-item-text">Aliquip elit labore non id nulla dolore magna in ea tempor. Ad aliquip mollit et ullamco dolore culpa aliquip ut tempor velit aliquip adipisicing. Consequat reprehenderit veniam dolor sint velit dolor.</p>
-                    <a href="#" className="case-item-link"></a>
-                </div>
-
-                <div className="case-item case-item-wide">
-                    <span className="button-image-overlay">View project</span>
-                    <h3 className="case-item-header">Project Title</h3>
-
-                    <p className="case-item-text">Exercitation proident cupidatat irure quis ex. Commodo consectetur duis ad proident non qui ipsum veniam ipsum eiusmod sint nostrud. Lorem nisi sint enim officia ut sint minim nostrud ea duis exercitation officia. Velit do qui sit excepteur veniam adipisicing laboris mollit. Officia ex exercitation sit laboris qui officia laborum esse labore consequat excepteur ullamco ullamco aliqua. Ullamco reprehenderit officia enim ullamco.</p>
-                    <a href="#" className="case-item-link"></a>
-                </div>
-
-                <div className="case-item case-item-gh-css">
-                    <span className="button-image-overlay">View project</span>
-                    <h3 className="case-item-header">Project Title</h3>
-                    <p className="case-item-text">Laborum incididunt fugiat velit cillum nostrud dolor amet. Sit occaecat irure deserunt minim est eiusmod eiusmod occaecat incididunt dolor cupidatat. Reprehenderit veniam excepteur duis id amet.</p>
-                    <div className="case-item-content">
-
-                    </div>
-                    <a href="#" className="case-item-link"></a>
-                </div>
-
-                <div data-image="#" className="case-item">
-                    <span className="button-image-overlay">View project</span>
-                    <h3 className="case-item-header">Project Title</h3>
-                    <p className="case-item-text">Laborum incididunt fugiat velit cillum nostrud dolor amet. Sit occaecat irure deserunt minim est eiusmod eiusmod occaecat incididunt dolor cupidatat. Reprehenderit veniam excepteur duis id amet.</p>
-                    <a href="#" className="case-item-link"></a>
-                </div>
-
-                <div data-image="#" className="case-item case-item-wide">
-                    <span className="button-image-overlay">View blog</span>
-                    <h3 className="case-item-header">Lorem magna aute Lorem commodo amet</h3>
-                    <p className="case-item-text">Anim incididunt mollit Lorem velit ut. Irure nostrud non non sint exercitation pariatur adipisicing aliqua aute esse adipisicing eiusmod consequat. Anim duis qui proident adipisicing.</p>
                     <a href="#" className="case-item-link"></a>
                 </div>
 
@@ -122,9 +66,9 @@ const Index = ({ posts = [] }) =>
                 <h2 className="section-header">Say hi.</h2>
 
                 <div className="contact-info">
-                    <a className="case-item-header" href="mailto:hello@ChrisBemister.com">hello@<br />ChrisBemister.com</a>
+                    <a className="case-item-header" href="mailto:chrisbemister@gmail.com">ChrisBemister<br />[at]gmail.com</a>
 
-                    <p className="case-item-text">I love to learn, develop, and hangout with my kids. I work as the <strong>Web Technologist</strong> for <strong><a href="https://autocan.ca"> AutoCanada</a></strong> <a href="https://twitter.com/ChrisBemister">Say hi!</a><br /><br />
+                    <p className="case-item-text">I love to learn, code, and hangout with my kids. I work as a <strong>Web Technologist</strong> for <strong><a href="https://autocan.ca"> AutoCanada</a></strong> <a href="https://twitter.com/ChrisBemister">Say hi!</a><br /><br />
 
                         Chris Bemister <br />Edmonton, Alberta</p>
 
