5608dadc2f679b8f4cfb1c153bad5b1e890972ad
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2018-08-01 21:59:50 -0600

    Removed sanity export

--- next.config.js
+++ next.config.js
@@ -1,31 +1,31 @@
 const client = require('./client')
 const withOffline = require('next-offline')
 
-module.exports = {
-  exportPathMap: async function (defaultPathMap) {
-    const path = await client
-      .fetch('*[_type == "post"].slug.current')
-      .then(data =>
-        data.reduce(
-          (acc, slug) => ({
-            '/': { page: '/' },
-            '/apps': { page: '/apps' },
-            '/stack': { page: '/stack' },
-            '/blog': { page: '/blog' },
-            '/profile': { page: '/profile' },
-            '/github': { page: '/github' },
-            '/contact': { page: '/contact' },
-            '/resume': { page: '/resume' },
-            ...acc,
-            [`/blog/${slug}`]: { page: '/post', query: { slug } }
-          }),
-          {}
-        )
-      )
-      .catch(console.error)
-    return path
-  }
-}
+// module.exports = {
+//   exportPathMap: async function (defaultPathMap) {
+//     const path = await client
+//       .fetch('*[_type == "post"].slug.current')
+//       .then(data =>
+//         data.reduce(
+//           (acc, slug) => ({
+//             '/': { page: '/' },
+//             '/apps': { page: '/apps' },
+//             '/stack': { page: '/stack' },
+//             '/blog': { page: '/blog' },
+//             '/profile': { page: '/profile' },
+//             '/github': { page: '/github' },
+//             '/contact': { page: '/contact' },
+//             '/resume': { page: '/resume' },
+//             ...acc,
+//             [`/blog/${slug}`]: { page: '/post', query: { slug } }
+//           }),
+//           {}
+//         )
+//       )
+//       .catch(console.error)
+//     return path
+//   }
+// }
 
 // const withSass = require('@zeit/next-sass')
 // module.exports = withSass()
