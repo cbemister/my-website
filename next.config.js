6079c7437d6689d778f782c2c769adf947d1a7d3
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2018-08-01 21:55:04 -0600

    Modified sw export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const client = require('./client')
 const withOffline = require('next-offline')
 
-module.exports.sanity = {
+module.exports = {
   exportPathMap: async function (defaultPathMap) {
     const path = await client
       .fetch('*[_type == "post"].slug.current')
@@ -30,30 +30,4 @@ module.exports.sanity = {
 // const withSass = require('@zeit/next-sass')
 // module.exports = withSass()
 
-module.exports.sw = withOffline(
-  {
-    exportPathMap: async function (defaultPathMap) {
-      const path = await client
-        .fetch('*[_type == "post"].slug.current')
-        .then(data =>
-          data.reduce(
-            (acc, slug) => ({
-              '/': { page: '/' },
-              '/apps': { page: '/apps' },
-              '/stack': { page: '/stack' },
-              '/blog': { page: '/blog' },
-              '/profile': { page: '/profile' },
-              '/github': { page: '/github' },
-              '/contact': { page: '/contact' },
-              '/resume': { page: '/resume' },
-              ...acc,
-              [`/blog/${slug}`]: { page: '/post', query: { slug } }
-            }),
-            {}
-          )
-        )
-        .catch(console.error)
-      return path
-    }
-  }
-)
+module.exports.sw = withOffline()
