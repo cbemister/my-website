e20d63b5eff6fdcee263c2bdcb5feb3d259ee2e8
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2018-08-04 20:46:22 -0600

    Set-up simple js menu

--- components/Header.js
+++ components/Header.js
@@ -18,30 +18,47 @@ export default ({ seoTitle, description, pageTitle, categories, date }) => {
                 }}
             />
 
- <div className="full-menu">
-            <div className="full-menu--middle">
-                <button className="menu-close">close</button>
+            <div className="full-menu">
+                <div className="full-menu--middle">
 
-                <ul className="main-nav">
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Work</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
+                    <ul class="main-nav">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Work</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+
+                    <div className="nav js-nav main-nav">
+                        <div className="nav-content">
+                            <div className="js-nav-header nav-header">
+                                <span className="nav-header-text">Chris Bemister</span>
+                                <div className="nav-header-line js-nav-header-line"></div>
+                            </div>
+
+                            <Navbar />
+
+                            <div className="nav-sublinks js-nav-animate">
+                                <div className="js-nav-animate">
+                                    <a className="nav-link nav-sublink" href="/github">Github</a>
+                                    <a className="nav-link nav-sublink" href="/contact">Contact</a>
+                                    <a className="nav-link nav-sublink" href="/resume">Resume</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
 
             </div>
 
-        </div>
+            {/* <button className="menu-toggle">Open full-screen navigation here</button> */}
 
-            <button className="menu-toggle">Open full-screen navigation here</button>
+            <a href="#" className="menu menu-toggle">
+                <span className="menu-icon-line-1 menu-icon-line"></span>
+                <span className="menu-icon-line-2 menu-icon-line"></span>
+                <span className="menu-icon-line-3 menu-icon-line"></span>
+            </a>
 
             <h1 className="site-name">
                 <a className="site-name-link" href="/">Chris Bemister</a>
@@ -52,34 +69,6 @@ export default ({ seoTitle, description, pageTitle, categories, date }) => {
 
             {/* {hasPageTitle} */}
 
-            {/* <div className="menu-animation-wrapper">
-                <div className="menu-animated-background"></div>
-            </div>
-            <a href="/" className="menu ">
-                <span className="menu-icon-line-1 menu-icon-line"></span>
-                <span className="menu-icon-line-2 menu-icon-line"></span>
-                <span className="menu-icon-line-3 menu-icon-line"></span>
-            </a>
-
-            <div className="nav js-nav">
-                <div className="nav-content">
-                    <div className="js-nav-header nav-header">
-                        <span className="nav-header-text">Chris Bemister</span>
-                        <div className="nav-header-line js-nav-header-line"></div>
-                    </div>
-
-                    <Navbar />
-
-                    <div className="nav-sublinks js-nav-animate">
-                        <div className="js-nav-animate">
-                            <a className="nav-link nav-sublink" href="/github">Github</a>
-                            <a className="nav-link nav-sublink" href="/contact">Contact</a>
-                            <a className="nav-link nav-sublink" href="/resume">Resume</a>
-                        </div>
-                    </div>
-                </div>
-
-            </div> */}
 
         </div>
 
--- static/js/menu.js
+++ static/js/menu.js
@@ -1,11 +1,10 @@
 const menuToggle = document.querySelector('.menu-toggle');
-const menuClose = document.querySelector('.menu-close');
 const fullMenu = document.querySelector('.full-menu');
 
 menuToggle.onclick = function () {
-    fullMenu.classList.toggle('full-menu--open');
-}
 
-menuClose.onclick = function () {
+    menuToggle.classList.contains('menu-active') ? this.classList.remove('menu-active') : this.classList.add('menu-active')
+
     fullMenu.classList.toggle('full-menu--open');
-}
+
+    }
