e00391df0574e65d323c46597e61a76b2af4a7aa
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2018-08-07 15:18:49 -0600

    Update Sub_navbar.js

--- components/navigation/Sub_navbar.js
+++ components/navigation/Sub_navbar.js
@@ -1,17 +1,18 @@
 import Link from 'next/link'
+import { navItem } from '../helpers/ClickListeners'
 
 export default () =>
 
     <div className="nav-sublinks js-nav-animate">
         <div className="js-nav-animate">
             <Link prefetch href="/github">
-                <a className="nav-link nav-sublink">Github</a>
+                <a onClick={ navItem } className="nav-link nav-sublink">Github</a>
             </Link>
             <Link prefetch href="/contact">
-                <a className="nav-link nav-sublink">Contact</a>
+                <a onClick={ navItem } className="nav-link nav-sublink">Contact</a>
             </Link>
             <Link prefetch href="/resume">
-                <a className="nav-link nav-sublink">Resume</a>
+                <a onClick={ navItem } className="nav-link nav-sublink">Resume</a>
             </Link>
 
 
