deca3d62600c276bf87ac876b23652b49a4da54b
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2018-08-07 22:16:03 -0600

    Fixed typo in link

--- components/navigation/Navbar.js
+++ components/navigation/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => (
     <ul className="nav-categories">
         <A href="/" className="nav-link" name="Home" />
         <A href="/apps" className="nav-link" name="Apps" />
-        <A href="/road" className="nav-link" name="Roadmap" />
+        <A href="/roadmap" className="nav-link" name="Roadmap" />
         {/* <A href="/stack" className="nav-link" name="Stack" /> */}
         {/* <A href="/blog" className="nav-link" name="Blog" /> */}
         {/* <A href="/metrics" className="nav-link" name="Metrics" /> */}
