079aee37f3fb0fc339f5a2377bca20f872ce54dd
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2018-08-25 16:07:41 -0600

    Removed extra code

--- next.config.js
+++ next.config.js
@@ -25,18 +25,3 @@ module.exports = withPlugins([
 
 // const withSass = require('@zeit/next-sass')
 // module.exports = withSass()
-
-
-// next.config.js
-module.exports = {
-  exportPathMap: function(defaultPathMap) {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/readme.md': { page: '/readme' },
-      '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
-      '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
-      '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } }
-    }
-  }
-}
