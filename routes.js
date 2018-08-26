23f14de0c44908dce13c1bb2aef95c40408d8b7f
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2018-08-25 21:55:25 -0600

    Set-up routes file

---
+++ routes.js
@@ -0,0 +1,27 @@
+const routes = module.exports = require('next-routes')()
+ 
+routes
+//.add('about')
+//.add('blog', '/blog/:slug')
+//.add('user', '/user/:id', 'profile')
+//.add('/:noname/:lang(en|es)/:wow+', 'complex')
+.add({name: '/', page: 'index'})
+.add({name: 'apps', page: 'apps'})
+.add({name: 'contact', page: 'contact'})
+.add({name: 'github', page: 'github'})
+.add({name: 'login', page: 'login'})
+.add({name: 'metrics', page: 'metrics'})
+.add({name: 'resume', page: 'resume'})
+.add({name: 'roadmap', page: 'roadmap'})
+.add({name: 'stack', page: 'stack'})
+
+
+// '/': { page: '/' },
+// '/apps': { page: '/apps' },
+// '/contact': { page: '/contact' },
+// '/github': { page: '/github' },
+// '/login ': { page: '/login' },
+// '/metrics ': { page: '/metrics' },
+// '/resume': { page: '/resume' },
+// '/roadmap': { page: '/roadmap' },
+// '/stack': { page: '/stack' }
