2d0c14adf1395b06eaa72c19cf51dce7c9368bc5
Chris Bemister <29780975+cbemister@users.noreply.github.com>
2018-08-27 21:55:28 -0600

    Reconfigured export

--- next.config.js
+++ next.config.js
@@ -1,11 +1,32 @@
 const withPlugins = require('next-compose-plugins');
 const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants');
 const withOffline = require('next-offline');
+const fetch = require('isomorphic-unfetch')
  
 module.exports = withPlugins([
   [withOffline, {
     exportPathMap: async function(defaultPathMap) {
-      return {
+  
+      // we fetch our list of posts, this allow us to dynamically generate the exported pages
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts?_page=1')
+      const postList = await response.json()
+
+      // tranform the list of posts into a map of pages with the pathname `/post/:id`
+      const pages = postList.reduce(
+        (pages, post) =>
+          Object.assign({}, pages, {
+            [`/post/${post.id}`]: {
+              page: '/post',
+              query: { id: post.id }
+            }
+          }),
+        {}
+      )
+
+      // combine the map of post pages with the home
+
+
+      return Object.assign({}, pages, {
         '/': { page: '/' },
         '/apps': { page: '/apps' },
         '/contact': { page: '/contact' },
@@ -15,7 +36,7 @@ module.exports = withPlugins([
         '/resume': { page: '/resume' },
         '/roadmap': { page: '/roadmap' },
         '/stack': { page: '/stack' }
-      }
+      })
     }
     }, ['!', PHASE_DEVELOPMENT_SERVER]],
 ]);
